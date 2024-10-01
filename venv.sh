c0405df99bb1dd8ec8accdb29d1525e5d8f0b215
morzen <morzen@kali.localdomain>
2024-10-01 17:33:52 +0100

    modifications to the script sadly it does not automatically log in the venv session it has to be done by hand

--- venv.sh
+++ venv.sh
@@ -8,14 +8,17 @@
 #to download the packages run once the venv environnement
 #pip3 install -r requirements.txt
 
+sudo apt install python3-virtualenv
 
 # Name of the virtual environment
 VENV_NAME="myenv"
+PYTHON_VERSION=python3.11
 
 # Check if venv exists, if not create it
 if [ ! -d "$VENV_NAME" ]; then
     echo "Creating virtual environment $VENV_NAME"
-    python3 -m venv $VENV_NAME
+    # python3 -m venv $VENV_NAME
+    virtualenv --python $PYTHON_VERSION $VENV_NAME
 fi
 
 # Check if requirements.txt exists, if not create it
@@ -30,4 +33,6 @@ fi
 
 # Activate the virtual environment
 echo "Activating virtual environment $VENV_NAME"
-source $VENV_NAME/bin/activate
+
+. $VENV_NAME/bin/activate
+
